b7edd7a28044ded26c84fd3233c4079052e54ef6
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-18 14:44:11 +0800

    登出功能

---
+++ logout.php
@@ -0,0 +1,31 @@
+﻿<?php session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>登出</title>
+
+</head>
+
+<body>
+
+<?php
+
+	//清空session，顯示已登出、回首頁按鈕
+
+	session_destroy();
+
+	echo "<center><h1>已登出</h1></center>";
+
+?>
+
+<br>
+	<center><input type="button" value="首頁" onclick="location.href=('index.php')" /></center>
+
+
+
+
+</body>
+
+</html>
